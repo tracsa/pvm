2b2ba458f7c3eeaaacb34d9b03eb1edc4f35a45c
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-03 09:52:38 -0500

    move the code to a more appropiate place

--- pvm/auth/base.py
+++ pvm/auth/base.py
@@ -17,3 +17,18 @@ class BaseAuthProvider:
 
     def authenticate(self, **credentials) -> BaseUser:
         raise NotImplementedError('Must be implemented in subclasses')
+
+
+class BaseHierarchyProvider:
+
+    def __init__(self, config):
+        self.config = config
+
+    def validate_user(self, user, **params):
+        ''' given a user, should rise an exception if the user does not match
+        the hierarchy conditions required via params '''
+        raise NotImplementedError('Must be implemented in subclasses')
+
+    def find_users(self, **params) -> [HierarchyUser]:
+        ''' given the params, retrieves the user identifiers that match them '''
+        raise NotImplementedError('Must be implemented in subclasses')

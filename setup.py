d92323462a0886beb312fc784e31283e717dd0b4
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-04 14:58:49 -0500

    maybe this is the way

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup
 
 setup(
     name='pvm',
-    version='0.2.3',
-    packages=['pvm'],
+    version='0.2.4',
+    packages=[
+        'pvm',
+        'pvm.http',
+        'pvm.auth',
+        'pvm.auth.backends',
+        'pvm.auth.hierarchy',
+    ],
     package_data={'pvm': ['grammars/*.g']},
     include_package_data=True,
     install_requires=[

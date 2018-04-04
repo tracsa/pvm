6a5bb2ea50bdf847a274f8929c541ad7d1306db7
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-04 14:32:31 -0500

    need to specify submodules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pvm',
-    version='0.2.2',
+    version='0.2.3',
     packages=['pvm'],
     package_data={'pvm': ['grammars/*.g']},
     include_package_data=True,

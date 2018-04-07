a102c993efebb83e68e20f99605d2f5969d165b3
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-07 13:50:47 -0500

    the right way of xml validation

---
+++ xml/validate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+fres=0
+
+for f in `ls xml/*.xml`; do
+    xmllint --noout --relaxng xml/process-spec.rng $f;
+    res=$?
+
+    if [ $res -ne 0 ]; then
+        fres=$res
+    fi
+done
+
+exit $fres

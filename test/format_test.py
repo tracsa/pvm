4836c1b64c0cb625352bac73e6522219a0dee114
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-13 08:24:44 -0500

    base tests for interpol names

---
+++ test/format_test.py
@@ -0,0 +1,41 @@
+from flask import json
+
+from cacahuate.models import Execution
+
+from .utils import make_auth, make_user
+
+
+def test_interpolated_name(client):
+    juan = make_user('juan', 'Juan')
+    name = 'Computes a name based on a Cow'
+
+    res = client.post('/v1/execution', headers={**{
+        'Content-Type': 'application/json',
+    }, **make_auth(juan)}, data=json.dumps({
+        'process_name': 'interpol',
+        'form_array': [{
+            'ref': 'form',
+            'data': {
+                'field': 'Cow',
+            },
+        }],
+    }))
+
+    # request succeeded
+    assert res.status_code == 201
+
+    # execution has name
+    exc = Execution.get_all()[0]
+
+    assert exc.name == name
+
+    # execution collection has name
+    reg2 = next(mongo[config["MONGO_EXECUTION_COLLECTION"]].find())
+
+    assert reg2['id'] == exc.id
+    assert reg2['name'] == name
+
+    # history has the name
+    reg = next(mongo[config["MONGO_HISTORY_COLLECTION"]].find())
+
+    assert reg['execution']['name'] == name

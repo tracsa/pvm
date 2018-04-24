e0b60be79f3182103d1cec789930e75b5bb016a6
Abraham Toriz <awonderfulcode@gmail.com>
2018-04-24 16:40:09 -0500

    delete topological sort code

--- cacahuate/xml.py
+++ cacahuate/xml.py
@@ -272,61 +272,6 @@ def get_form_specs(node):
     return specs
 
 
-@comment
-def etree_from_list(root: Element, nodes: [Element]) -> 'ElementTree':
-    ''' Returns a built ElementTree from the list of its members '''
-    root = Element(root.tag, attrib=root.attrib)
-    root.extend(nodes)
-
-    return ElementTree(root)
-
-
-@comment
-def nodes_from(node: Element, graph):
-    ''' returns an iterator over the (node, edge)s that can be reached from
-    node '''
-    for edge in graph.findall(".//*[@from='{}']".format(node.attrib['id'])):
-        yield (graph.find(".//*[@id='{}']".format(edge.attrib['to'])), edge)
-
-
-@comment
-def has_no_incoming(node: Element, graph: 'root Element'):
-    ''' returns true if this node has no edges pointing to it '''
-    return len(graph.findall(".//*[@to='{}']".format(node.attrib['id']))) == 0
-
-
-@comment
-def has_edges(graph: 'root Element'):
-    ''' returns true if the graph still has edge elements '''
-    return len(graph.findall("./connector")) > 0
-
-
-@comment
-def topological_sort(start_node: Element, graph: 'Element') -> 'ElementTree':
-    ''' sorts topologically the given xml element tree, source:
-    https://en.wikipedia.org/wiki/Topological_sorting '''
-    sorted_elements = []  # Empty list that will contain the sorted elements
-    no_incoming = [(start_node, None)]  # (node, edge that points to this node)
-
-    while len(no_incoming) > 0:
-        node, edge = no_incoming.pop()
-
-        if edge is not None:
-            sorted_elements.append(edge)
-        sorted_elements.append(node)
-
-        for m, edge in nodes_from(node, graph=graph):
-            graph.remove(edge)
-
-            if has_no_incoming(m, graph):
-                no_incoming.append((m, edge))
-
-    if has_edges(graph) > 0:
-        raise Exception('graph is cyclic')
-
-    return etree_from_list(graph, sorted_elements)
-
-
 SUPPORTED_ATTRS = {
     'default': str,
     'helper': str,

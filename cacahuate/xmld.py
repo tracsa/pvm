9ff11c6ab57dded10bbe23ba6e5a2c34ae029222
Ramiro <rgutierrez@tracsa.com.mx>
2018-05-29 11:26:02 -0500

    command validate condition.xml

--- cacahuate/xmld.py
+++ cacahuate/xmld.py
@@ -5,51 +5,74 @@ from cacahuate.xml import Xml
 from cacahuate.http.wsgi import app, mongo
 from xml.dom import pulldom
 from cacahuate.grammar import Condition
-
-ids = []
-errores = {'1':'id repetido'}
-
+from cacahuate.xml import get_text
+from cacahuate.xml import NODES
 
 def main():
+    ids = []
     file = sys.argv[1]
-    nodos = ['action', 'call','validation', 'exit', 'request']
+    data_form = {}
+    param_auth_filter = {}
+    conditions = []
+
+    nodos = list(NODES)
+
+    def check_id(node):
+        if node.getAttribute('id'):
+            id_element = node.getAttribute('id')
+
+            if not id_element in ids:
+                ids.append(id_element)
+            else:
+                sys.exit("Error id: '{}' repeat in '{}' ".format(id_element, file))
+
 
     doc = pulldom.parse('xml/{}'.format(file))
-    # for event, node in doc:
-    #     print (event)
-    #     print (node)
-        # if event == pulldom.START_ELEMENT and node.tagName in nodos:
-        #     doc.expandNode(node)
-        #     check_id(node)
+    for event, node in doc:
 
-        # elif  event == pulldom.START_ELEMENT and node.tagName == 'if':
-        #     check_id(node)
+        if event == pulldom.START_ELEMENT and node.tagName in nodos and not node.tagName == 'if' :
+            check_id(node)
+            doc.expandNode(node)
+            if node.tagName == 'action':
+                if node.getElementsByTagName("form"):
+                    form_id = node.getElementsByTagName("form")[0].getAttribute('id')
+                    inputs = node.getElementsByTagName("input")
+                    array_input = {}
+                    for inpt in inputs:
+                        array_input[inpt.getAttribute('name')] = inpt.getAttribute('default')
+                    data_form[form_id] = array_input
 
-        # elif  event == pulldom.START_ELEMENT and node.tagName == 'condition':
-        #     check_id(node)
-        #     doc.expandNode(node)
-        #     condition = get_text(node)
-        #     grammar = Condition(state['state'])
+                params = node.getElementsByTagName("param")
+                for param in params:
+                    if param.getAttribute('type'):
+                        if param.getAttribute('type') == 'ref':
+                            reference_form = (get_text(param).split('#')[1]).split('.')[0]
+                            field_form = get_text(param).split('#')[1].split('.')[1]
+                            try:
+                                data_form[reference_form][field_form]
+                            except:
+                                sys.exit("Not found param '{}' in 'form#{}' in '{}' ".format(field_form, reference_form, file))
 
-        #     value = grammar.parse(condition)
+            if node.tagName == 'validation':
+                deps = node.getElementsByTagName("dep")
+                for dep in deps:
+                    form, field = get_text(dep).split('.')
+                    try:
+                        data_form[form][field]
+                    except:
+                        sys.exit("Not found dependence 'form#{}.{}' in {}".format(form, field, file))
 
+        if event == pulldom.START_ELEMENT and node.tagName == 'if':
+            check_id(node)
 
-def check_id(node):
-    if node.getAttribute('id'):
-        id_element = node.getAttribute('id')
-        if not id_element in ids:
-            ids.append(id_element)
-        else:
-            print (id_element, ' repetido')
-            sys.exit(1)
+        if event == pulldom.START_ELEMENT and node.tagName == 'condition':
+            doc.expandNode(node)
+            conditions.append(get_text(node))
 
+    con = Condition(data_form)
+    for condition in conditions:
+        if not con.parse(condition):
+            sys.exit("Error '{}' in  '{}' ".format(condition, file))
 
 if __name__ == '__main__':
     main()
-
-
-def get_text(node):
-    node.normalize()
-    if node.firstChild is not None:
-        return node.firstChild.nodeValue or ''
-    return ''

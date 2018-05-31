918dc839f8575c35fcbefabdfbeeba975d883e34
Abraham Toriz <awonderfulcode@gmail.com>
2018-05-30 22:13:13 -0500

    show the filename that is failing

--- test/scripts_test.py
+++ test/scripts_test.py
@@ -9,24 +9,28 @@ def test_xml_validates():
 def test_id_repeat():
     with pytest.raises(SystemExit) as cm:
         xml_validate('xml/condition_id_repeat.2018-05-28.xml')
-    assert str(cm.value) == "Duplicated id: 'start-node'"
+    assert str(cm.value) == \
+        "xml/condition_id_repeat.2018-05-28.xml: Duplicated id: 'start-node'"
 
 
 def test_not_param():
     with pytest.raises(SystemExit) as cm:
         xml_validate('xml/condition_not_param.2018-05-28.xml')
     assert str(cm.value) == \
-        "Referenced param does not exist 'a.b'"
+        "xml/condition_not_param.2018-05-28.xml: Referenced param does not " \
+        "exist 'a.b'"
 
 
 def test_not_dep():
     with pytest.raises(SystemExit) as cm:
         xml_validate('xml/condition_not_dep.2018-05-28.xml')
     assert str(cm.value) == \
-        "Referenced dependency does not exist 'a.b'"
+        "xml/condition_not_dep.2018-05-28.xml: Referenced dependency does " \
+        "not exist 'a.b'"
 
 
 def test_not_valid():
     with pytest.raises(SystemExit) as cm:
         xml_validate('xml/condition_not_valid.2018-05-28.xml')
-    assert str(cm.value) == 'Lex error in condition'
+    assert str(cm.value) == \
+        'xml/condition_not_valid.2018-05-28.xml: Lex error in condition'

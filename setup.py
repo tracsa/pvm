8ae227d2e2b05d514f577bb2e1e4433366852148
Abraham Toriz <awonderfulcode@gmail.com>
2018-06-12 15:49:10 -0500

    release v2.3.0 with new api for auth and hierarchy providers

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     url='https://github.com/tracsa/cacahuate',
 
-    version='2.2.1',
+    version='2.3.0',
 
     author='Abraham Toriz Cruz',
     author_email='categulario@gmail.com',

1c232b8ffac62ef784660a4639027ae11639b094
Abraham Toriz <awonderfulcode@gmail.com>
2018-07-13 10:13:23 -0500

    formalization of tests for if elif else

--- test/handler_test.py
+++ test/handler_test.py
@@ -1639,7 +1639,116 @@ def test_anidated_conditions(config, mongo):
     assert ptr.node_id == 'g'
 
 
-def test_else_statement(config, mongo):
+def test_ifelifelse_if(config, mongo):
+    ''' else will be executed if preceding condition is false'''
+    # test setup
+    handler = Handler(config)
+    user = make_user('juan', 'Juan')
+    ptr = make_pointer('else.2018-07-10.xml', 'start_node')
+    channel = MagicMock()
+
+    mongo[config["EXECUTION_COLLECTION"]].insert_one({
+        '_type': 'execution',
+        'id': ptr.proxy.execution.get().id,
+        'state': Xml.load(config, 'else').get_state(),
+    })
+
+    handler.call({
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'user_identifier': user.identifier,
+        'input': [
+            {
+                'ref': 'secret01',
+                '_type': 'form',
+                'inputs': {
+                    '_type': ':sorted_map',
+                    'item_order': [
+                        'password',
+                    ],
+                    'items': {
+                        'password': {
+                            'type': 'text',
+                            'value': 'incorrect!',
+                        },
+                    },
+                },
+            },
+        ],
+    }, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'condition01'
+
+    # rabbit called
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    rabbit_call = {
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'input': [{
+            '_type': 'form',
+            'ref': 'condition01',
+            'state': 'valid',
+            'inputs': {
+                '_type': ':sorted_map',
+                'items': {
+                    'condition': {
+                        'name': 'condition',
+                        'state': 'valid',
+                        'type': 'bool',
+                        'value': True,
+                    },
+                },
+                'item_order': ['condition'],
+            },
+        }],
+        'user_identifier': '__system__',
+    }
+    assert json.loads(args['body']) == rabbit_call
+
+    channel = MagicMock()
+    handler.call(rabbit_call, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'action01'
+
+    # rabbit called to notify the user
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    assert args['exchange'] == 'cacahuate_notify'
+
+    channel = MagicMock()
+    handler.call({
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'user_identifier': user.identifier,
+        'input': [{
+            'ref': 'form01',
+            '_type': 'form',
+            'inputs': {
+                '_type': ':sorted_map',
+                'item_order': ['answer'],
+                'items': {
+                    'answer': {
+                        'name': 'answer',
+                        'value': 'answer',
+                    },
+                },
+            },
+        }],
+    }, channel)
+
+    # execution finished
+    assert len(Pointer.get_all()) == 0
+    assert len(Execution.get_all()) == 0
+
+
+def test_ifelifelse_elif(config, mongo):
     ''' else will be executed if preceding condition is false'''
     # test setup
     handler = Handler(config)
@@ -1709,12 +1818,222 @@ def test_else_statement(config, mongo):
     }
     assert json.loads(args['body']) == rabbit_call
 
+    channel = MagicMock()
     handler.call(rabbit_call, channel)
 
     # pointer moved
     assert Pointer.get(ptr.id) is None
     ptr = Pointer.get_all()[0]
     assert ptr.node_id == 'elif01'
+
+    # rabbit called
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    rabbit_call = {
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'input': [{
+            '_type': 'form',
+            'ref': 'elif01',
+            'state': 'valid',
+            'inputs': {
+                '_type': ':sorted_map',
+                'items': {
+                    'condition': {
+                        'name': 'condition',
+                        'state': 'valid',
+                        'type': 'bool',
+                        'value': True,
+                    },
+                },
+                'item_order': ['condition'],
+            },
+        }],
+        'user_identifier': '__system__',
+    }
+    assert json.loads(args['body']) == rabbit_call
+
+    channel = MagicMock()
+    handler.call(rabbit_call, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'action02'
+
+    # rabbit called to notify the user
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    assert args['exchange'] == 'cacahuate_notify'
+
+    channel = MagicMock()
+    handler.call({
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'user_identifier': user.identifier,
+        'input': [{
+            'ref': 'form01',
+            '_type': 'form',
+            'inputs': {
+                '_type': ':sorted_map',
+                'item_order': ['answer'],
+                'items': {
+                    'answer': {
+                        'name': 'answer',
+                        'value': 'answer',
+                    },
+                },
+            },
+        }],
+    }, channel)
+
+    # execution finished
+    assert len(Pointer.get_all()) == 0
+    assert len(Execution.get_all()) == 0
+
+
+def test_ifelifelse_else(config, mongo):
+    ''' else will be executed if preceding condition is false'''
+    # test setup
+    handler = Handler(config)
+    user = make_user('juan', 'Juan')
+    ptr = make_pointer('else.2018-07-10.xml', 'start_node')
+    channel = MagicMock()
+
+    mongo[config["EXECUTION_COLLECTION"]].insert_one({
+        '_type': 'execution',
+        'id': ptr.proxy.execution.get().id,
+        'state': Xml.load(config, 'else').get_state(),
+    })
+
+    handler.call({
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'user_identifier': user.identifier,
+        'input': [
+            {
+                'ref': 'secret01',
+                '_type': 'form',
+                'inputs': {
+                    '_type': ':sorted_map',
+                    'item_order': [
+                        'password',
+                    ],
+                    'items': {
+                        'password': {
+                            'type': 'text',
+                            'value': 'cuca',
+                        },
+                    },
+                },
+            },
+        ],
+    }, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'condition01'
+
+    # rabbit called
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    rabbit_call = {
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'input': [{
+            '_type': 'form',
+            'ref': 'condition01',
+            'state': 'valid',
+            'inputs': {
+                '_type': ':sorted_map',
+                'items': {
+                    'condition': {
+                        'name': 'condition',
+                        'state': 'valid',
+                        'type': 'bool',
+                        'value': False,
+                    },
+                },
+                'item_order': ['condition'],
+            },
+        }],
+        'user_identifier': '__system__',
+    }
+    assert json.loads(args['body']) == rabbit_call
+
+    channel = MagicMock()
+    handler.call(rabbit_call, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'elif01'
+
+    # rabbit called
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    rabbit_call = {
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'input': [{
+            '_type': 'form',
+            'ref': 'elif01',
+            'state': 'valid',
+            'inputs': {
+                '_type': ':sorted_map',
+                'items': {
+                    'condition': {
+                        'name': 'condition',
+                        'state': 'valid',
+                        'type': 'bool',
+                        'value': False,
+                    },
+                },
+                'item_order': ['condition'],
+            },
+        }],
+        'user_identifier': '__system__',
+    }
+    assert json.loads(args['body']) == rabbit_call
+
+    channel = MagicMock()
+    handler.call(rabbit_call, channel)
+
+    # pointer moved
+    assert Pointer.get(ptr.id) is None
+    ptr = Pointer.get_all()[0]
+    assert ptr.node_id == 'action03'
+
+    # rabbit called to notify the user
+    channel.basic_publish.assert_called_once()
+    args = channel.basic_publish.call_args[1]
+    assert args['exchange'] == 'cacahuate_notify'
+
+    channel = MagicMock()
+    handler.call({
+        'command': 'step',
+        'pointer_id': ptr.id,
+        'user_identifier': user.identifier,
+        'input': [{
+            'ref': 'form01',
+            '_type': 'form',
+            'inputs': {
+                '_type': ':sorted_map',
+                'item_order': ['answer'],
+                'items': {
+                    'answer': {
+                        'name': 'answer',
+                        'value': 'answer',
+                    },
+                },
+            },
+        }],
+    }, channel)
+
+    # execution finished
+    assert len(Pointer.get_all()) == 0
+    assert len(Execution.get_all()) == 0
 
 
 def test_exit_interaction(config, mongo):

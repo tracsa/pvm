c4c41b185cccb3b52a95a1e64ba20efc06ef8521
Kevin Villalobos <dracojan_12@hotmail.com>
2020-06-19 05:38:41 -0500

    Verify reject is a non empty list

--- cacahuate/node.py
+++ cacahuate/node.py
@@ -526,7 +526,10 @@ class Validation(UserAttachedNode):
             if 'inputs' not in json_data:
                 raise RequiredInputError('inputs', 'request.body.inputs')
 
-            if type(json_data['inputs']) is not list:
+            if any([
+                type(json_data['inputs']) is not list,
+                len(json_data['inputs']) == 0,
+            ]):
                 raise RequiredListError('inputs', 'request.body.inputs')
 
             for index, field in enumerate(json_data['inputs']):
